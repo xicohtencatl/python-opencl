6cd75574490948f87673c5510649fbdeb089b0e2
Xīcòtēncatl <xicohtencatl@next-touch.com>
2009-05-30 13:37:02 +0200

    Correcting Cython bug.

--- examples/ocl_bandwidth_test.py
+++ examples/ocl_bandwidth_test.py
@@ -3,4 +3,8 @@
 import opencl
 
 if __name__ == '__main__':
-    devices = opencl.get_devices()
+    devices = opencl.get_device_ids(opencl.PLATFORM_NVIDIA,
+                                    opencl.DEVICE_TYPE_GPU)
+    print 'Running on...'
+    for dev in devices:
+        print ' Device %s (from %d)' % (opencl.device_name(dev), dev)
